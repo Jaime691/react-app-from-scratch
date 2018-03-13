26f66eee48ae2ebef36c01a49c44baaf45b1ca48
jmayorga <jmayorgadev@gmail.com>
2018-03-13 15:15:26 -0500

    tutorial completed

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const Index = () => {
+  return <div className="title">Hello React!</div>;
+};
+
+ReactDOM.render(<Index />, document.getElementById('index'));
